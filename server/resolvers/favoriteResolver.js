0c05eeb288a7f005d9068b265fed77961a2043ea
Temitope <tope_olajide@outlook.com>
2020-07-09 03:51:04 +0100

    Create fetchAllFavoriteProduct Resolver

--- server/resolvers/favoriteResolver.js
+++ server/resolvers/favoriteResolver.js
@@ -45,4 +45,32 @@ export default {
         }
       },
     },
+    Query: {
+      fetchAllFavoriteProduct: async (
+      parent,
+      { data },
+      { models: { favoriteModel,productModel }, user },
+      info
+    ) => {
+      if (!user) {
+        throw new AuthenticationError("You are not authenticated");
+      }
+      try {
+        const favoriteProducts = await favoriteModel.find({userId: user.id});
+        if (favoriteProducts) {
+        const favoriteProductsIds = favoriteProducts.map((products)=>{
+            return products.productId
+        })
+        console.log(favoriteProductsIds)
+        const records = await productModel.find().where('_id').in(favoriteProductsIds).exec();
+        console.log(records)
+        return records
+        }
+        return []
+      } catch (error) {
+        throw error;
+      }
+    },
+    
+  }
   }
