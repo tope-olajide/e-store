decf7aebf07beed0dc56cfbca9243e8111b4d9c0
Temitope <tope_olajide@outlook.com>
2020-07-20 11:51:47 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+
 import { Route, Switch } from "react-router-dom";
-import SignUp from './Components/SignUp'
-import SignIn from './Components/SignIn'
+import SignUp from "./Components/SignUp";
+import SignIn from "./Components/SignIn";
+import AllProducts from "./Components/AllProducts";
+
+library.add(faHeart, faHeartRegular);
+
 function App() {
   return (
     <>
-<Switch>
-    <Route exact path="/sign-up" component={SignUp} />
-    <Route exact path="/sign-in" component={SignIn} />
-</Switch>
+      <Switch>
+        <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/" component={AllProducts} />
+      </Switch>
     </>
   );
 }
