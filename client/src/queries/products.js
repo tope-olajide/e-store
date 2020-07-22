30657232d10a9830f4c46302c1ab4a64ab86b5c5
Temitope <tope_olajide@outlook.com>
2020-07-22 03:07:22 +0100

    Add GET_PRODUCT_DETAILS query

--- client/src/queries/products.js
+++ client/src/queries/products.js
@@ -1,19 +1,27 @@
 import gql from "graphql-tag";
 
 export const GET_ALL_PRODUCTS = gql`
-   {allProducts{
-        product{
-          _id
-          name
-          summary
-          price
-          createdAt
-          updatedAt
-          description
-          category
-          productImageUrl
-      },
+  {
+    allProducts {
+      product {
+        _id
+        name
+        price
+        productImageUrl
+      }
       isUserFavorite
+    }
+  }
+`;
+export const GET_PRODUCT_DETAILS = gql`
+  query ProductDetails($productId: ID!) {
+    productDetails(productId: $productId) {
+      name
+      summary
+      price
+      description
+      category
+      productImageUrl
     }
   }
 `;
